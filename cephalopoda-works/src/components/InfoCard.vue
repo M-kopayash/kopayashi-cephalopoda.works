d607677fc834becd36884423320e2c6068b3170a
kopa <miyabiya.jpn@gmail.com>
2023-05-13 17:24:48 +0900

    InfoCardコンポーネントを作成

---
+++ cephalopoda-works/src/components/InfoCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="px-4 text-left max-w-[650px] mx-auto ">
+        <h3 class="mt-4 font-bold" :class="titleClass">{{ title }}</h3>
+        <p class="mt-4" :class="contentClass">{{ content }}</p>
+        <hr class="mt-2">
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'InfoCard',
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        titleClass: {
+            type: String,
+        },
+        contentClass: {
+            type: String,
+        },
+    },
+});
+</script>
+<style scoped></style>
