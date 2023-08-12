3077756bccc0a5e89d9ba92ac6b8a1e6dd06f3c7
kopayashi <miyabiya.jpn@gmail.com>
2023-08-12 11:00:40 +0900

    Revert "test commit"
    This reverts commit d2e38ea9730d8f1be4b15c3892567e16411bc8f4.

---
+++ cephalopoda-works/src/components/ToggleCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="px-4 text-left max-w-[650px] mx-auto">
+    <h3
+      class="mt-4 font-bold"
+      :class="[titleClass, { clickable: !isPhone }]"
+      @click="toggleContent()">
+      {{ title }}
+    </h3>
+    <div v-if="isContentOpen || !isPhone">
+      <p class="mt-4" :class="contentClass" v-html="formattedContent"></p>
+      <hr class="mt-2 mb-4" :class="hrClass"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ToggleCard",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    titleClass: {
+      type: String,
+    },
+    contentClass: {
+      type: String,
+    },
+    hrClass: {
+      type: String,
+    }
+  },
+  computed: {
+    // 改行コードを改行タグに変更
+    formattedContent() {
+      return this.content.replace(/\r?\n|\r/g, '</p><p class="mt-4">');
+    },
+  },
+  data() {
+    return {
+      isContentOpen: false,
+      isPhone: false,
+    };
+  },
+  mounted() {
+    this.isPhone = window.innerWidth <= 768; // 768px以下の場合はスマフォとする
+  },
+  methods: {
+    toggleContent() {
+      this.isContentOpen = !this.isContentOpen;
+    },
+  },
+});
+
+</script>
+<style scoped></style>
---
+++ cephalopoda-works/src/views/HomePlace.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <p class="mb-8">これから書き足していきます。ボチボチやっていきます。</p>
+    <img src="../assets/images/1373.jpg" alt="squid-1373" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomePlace",
+});
+</script>
